6fdda940b5d118a161892f9fbadd9269bf5515bc
douglasparrales <douglas.app.26@gmail.com>
2025-08-15 23:41:13 -0500

    feat: pathvariable
    feat: add anottation @PathVariable

---
+++ src/main/java/SpringBoot/controller/GreetingsController.java
@@ -0,0 +1,14 @@
+package SpringBoot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping({"/saludo/{name}", "/hola/{name}", "/greetings/{name}"})
+    public String greetings(@PathVariable String name) {
+        return "Hola " + name;
+    }
+}
