6801c24a22c94f3cee2e3f2cef2a2e1461505d9d
douglasparrales <douglas.app.26@gmail.com>
2025-08-19 18:14:53 -0500

    chore: comments
    chore: comments

--- src/main/java/SpringBoot/controller/DeletreoController.java
+++ src/main/java/SpringBoot/controller/DeletreoController.java
@@ -4,9 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controlador para deletrear una palabra
+ */
 @RestController
 public class DeletreoController {
 
+    /**
+     * Endpoint para deletrear una palabra
+     * @param word la palabra a deletrar
+     * @return array de cada letra de la palabra
+     */
     @GetMapping({"/deletrear/{word}", "/deletreo/{word}"})
     public String[] Deletreo(@PathVariable String word){
         String[] letras = new String[word.length()];
--- src/main/java/SpringBoot/controller/PalindromoController.java
+++ src/main/java/SpringBoot/controller/PalindromoController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class PalindromoController {
 
     @GetMapping({"/palindromo/{word}","/validar-palindromo/{word}"})
     public String Palindromo(@PathVariable String word){
         int lenght = word.length();
-        
         return "";
     }
 }
